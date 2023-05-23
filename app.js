7d31a26b5835c374522cf087f3126dc554985f0c
SSDUMA11 <dumasofia2@gmail.com>
2023-05-23 12:53:38 +0300

    task_2

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-import { promises, Stats } from 'fs';
+import { promises } from 'fs';
+
+const users = [
+  { name: 'Mike', age: 25 },
+  { name: 'Bob', age: 32 },
+  { name: 'Nikola', age: 17 }
+];
 
 const newData = [
   { name: 'Anna', age: 24 },
@@ -7,7 +13,7 @@ const newData = [
 
 const isExist = async (filePath) => {
   try {
-    await promises.stat(filePath, Stats);
+    await promises.stat(filePath);
     return true;
   } catch (error) {
     return false;
@@ -19,10 +25,12 @@ const isExist = async (filePath) => {
 
   if (await isExist(filePath)) {
     const data = await promises.readFile(filePath, { encoding: 'utf-8' });
-    const users = JSON.parse(data);
-    users.users.push(...newData);
+    const usersData = JSON.parse(data);
+    usersData.dataUsers.push(...users);
+    usersData.dataUsers.push(...newData);
+    console.log(usersData.dataUsers);
 
-    const updatedData = JSON.stringify(users);
+    const updatedData = JSON.stringify(usersData);
     await promises.writeFile(filePath, updatedData, { encoding: 'utf-8' });
 
     console.log('Data has been saved to data.json');
